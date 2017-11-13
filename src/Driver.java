620df4269bf5017186d0d17d15756af5284aad71
Hamid Zare <dellydela@gmail.com>
2017-11-12 21:29:37 -0600

    added simple tests to test the Heap

--- src/Driver.java
+++ src/Driver.java
@@ -4,10 +4,23 @@ import java.util.Arrays;
 public class Driver {
     public static void main(String[] args) {
         int[] testArray = new int[] {5,2,3,4,-4,5, 6, 17, 3, -2, 8};
-        Sorter heapSort = new HeapSort();
 
-        Heap binaryHeapInPlace = new Heap(testArray, 2, false, false);
-        heapSort.time(testArray);
+
+        // check heap
+        Heap testHeap = new Heap(testArray,2,true, true);
+        try {
+            if (testHeap.getRoot() != 17)
+                throw new Exception("bad getRoot");
+            if (testHeap.extractRoot() != 17)
+                throw new Exception("bad extract");
+            if (testHeap.extractRoot() != 8)
+                throw new Exception("didn't bubble down properly");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Sorter heapSort = new HeapSort(testArray, 2);
+        heapSort.time();
 
     }
 }
