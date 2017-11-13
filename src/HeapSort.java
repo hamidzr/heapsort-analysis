7a39583ae45b6d554e56ff019a8103040f343436
Hamid Zare <dellydela@gmail.com>
2017-11-12 21:32:30 -0600

    simplified heapsort as heap has it's own datastrcure now

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -1,55 +1,26 @@
 public class HeapSort extends Sorter {
-    private int n = 2;
+    private int[] input;
+    private int numChildren = 2;
 
-    public HeapSort(int n){
-        this.n = n;
+    public HeapSort(int[] numbers){
+        input = numbers;
     }
 
-    public HeapSort(){
+    public HeapSort(int[] numbers, int numChildren){
+        this.numChildren = numChildren;
+        input = numbers;
     }
 
     @Override
-    public void sort(int[] numbers) {
-        int size = numbers.length;
+    public void sort() {
+        // create the heap
+        Heap mHeap = new Heap(input, numChildren, true, true);
 
-        // build the heap inplace
-        for (int i = size / 2 - 1; i >= 0; i--)
-            heapify(numbers, size, i);
-
-        // extract max
-        for (int i=size-1; i>=0; i--)
-        {
-            // move root to end
-            swap(numbers, 0, i);
-            heapify(numbers, i, 0);
+        // sort
+        int size = mHeap.size();
+        for (int i = 0; i < size; i++) {
+            mHeap.extractRoot();
         }
-    }
 
-    public void heapify(int[] arr, int heapSize, int rootIndex) {
-        int largest = rootIndex;
-        int left = 2*rootIndex + 1;
-        int right = 2*rootIndex + 2;
-
-        // left > largest = root
-        if (left < heapSize && arr[left] > arr[largest])
-            largest = left;
-
-        // right > largest
-        if (right < heapSize && arr[right] > arr[largest])
-            largest = right;
-
-        // If largest is not root
-        if (largest != rootIndex)
-        {
-            swap(arr, rootIndex, largest);
-            heapify(arr, heapSize, largest);
-        }
-    }
-
-    // swap elements in a list
-    private void swap(int[] array, int i, int j) {
-        int tmp = array[i];
-        array[i] = array[j];
-        array[j] = tmp;
     }
 }
