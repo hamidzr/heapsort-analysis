6f7f980490a93e7db99b91ad4857818b0ce0cad1
Hamid Zare <dellydela@gmail.com>
2017-12-06 10:00:18 -0600

    added outputting of figures to figures dir

--- src/analyze.py
+++ src/analyze.py
@@ -5,13 +5,19 @@ import numpy as np
 import os
 import subprocess
 import math
+import time
 import re
 from tqdm import tqdm
 
 JAVA = '/usr/bin/java'
 JVM_INIT_MEMORY = '500m'
 JVM_MAX_MEMORY = '4g'
-EAT_MEMORY = "a" * int(2 * math.pow(10,9))
+EAT_MEMORY = "a" * int(8 * math.pow(10,9))
+
+START_SIZE = 1000
+GROWTH_RATE = 5000
+NUM_TESTS = 50
+COMMENT = 'swap'
 
 def testWith(size, maxNum):
     cmd = f'{JAVA} -Xms{JVM_INIT_MEMORY} -Xmx{JVM_MAX_MEMORY} Driver -array_size {size} -max_number {maxNum}'
@@ -57,6 +63,9 @@ def analyze(configs):
         timesMatrix[idx] = times
     return timesMatrix
 
+def configsToString():
+    return f'{START_SIZE}-{GROWTH_RATE}-{NUM_TESTS}-{COMMENT}-R{time.time()}'
+
 def smooth(xs, ys):
     xs_smooth = np.linspace(xs.min(), xs.max(), 300)
     ys_smooth = spline(xs, ys, xs_smooth)
@@ -69,9 +78,6 @@ def plot(configs, times, names, xAxis):
     elif (xAxis == 'range'): #chart by range
         xs = np.array(list(map(lambda c: c[1]/1000, configs)))
 
-
-
-
     for y in range(0,len(names)):
         ys = np.array(list(map(lambda nano: nano/math.pow(10,6), times[:,y])))
         if (xAxis):
@@ -81,17 +87,18 @@ def plot(configs, times, names, xAxis):
             xs = np.arange(0,len(configs),1)
             xs_smooth, ys_smooth = smooth(xs, ys)
             plt.plot(xs_smooth, ys_smooth)
-    plt.title(f'Comparison')
+    title = 'Comparison' if (GROWTH_RATE != 0) else f'Comparison {int(START_SIZE/1000)}k'
+    plt.title(title)
     plt.ylabel('Time (ms)')
     if (xAxis): plt.xlabel(f'Input Size {xAxis}(k)')
     plt.legend(names, loc='upper left');
-    # plt.savefig(constants.RESULTS_DIR + f'/acc-{configToString()}.png', bbox_inches='tight')
+    plt.savefig(f'../figures/{configsToString()}.png', bbox_inches='tight')
     plt.show()
     plt.close() # clear fig
 
 sortNames = testWith(1,1)[0]
-configs = generateConfigs(startSize=20000000, growthRate=0, numTests=10)
+configs = generateConfigs(startSize=START_SIZE, growthRate=GROWTH_RATE, numTests=NUM_TESTS)
 times = analyze(configs)
 plot(configs, times, sortNames, xAxis='size')
 
-EAT_MEMORY = EAT_MEMORY + 1 # keep the it in memory 
+EAT_MEMORY = EAT_MEMORY + 'a' # keep the it in memory 
